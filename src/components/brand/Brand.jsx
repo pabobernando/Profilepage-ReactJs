c6effb6f2c36a8e7da8f361fb3b34ab14a5111d0
pabobernando <pabobernando69@gmail.com>
2022-01-12 15:34:52 +0700

    end brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { google, slack, atlassian, dropbox,shopify } from './imports'
-import './brand.css'
+import React from 'react';
+import { google, slack, atlassian, dropbox,shopify } from './imports';
+import './brand.css';
 
 const Brand = () => {
     return (
         <div className='gpt3__brand section__padding'>
-            <div><img src="{google}" alt="google" srcset="" /></div>
-        </div>
+            <div>
+                <img src={google} alt="google" srcset="" />
+            </div>
+            <div>
+                <img src={slack} alt='slack' srcset="" />
+            </div>
+            <div>
+                <img src={atlassian} alt='atlasian' srcSet='' />
+            </div>
+            <div>
+                <img src={dropbox} alt='dropbox' srcSet='' />
+            </div>
+            <div>
+                <img src={shopify} alt='shopify' srcSet='' />
+            </div>
+            </div>
     )
 }
 
